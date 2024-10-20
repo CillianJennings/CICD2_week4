4210716ec824b68afd3a208a9fed4f4b669742e0
Cillian <G00404411@atu.ie>
2024-10-20 18:43:58 +0100

    Small changes

--- src/main/java/ie/atu/week4/jpa/ProductService.java
+++ src/main/java/ie/atu/week4/jpa/ProductService.java
@@ -3,17 +3,40 @@ package ie.atu.week4.jpa;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductService {
-    private ProductRep productRep;
+    private final ProductRep productRep;
 
     public ProductService(ProductRep productRep) {
         this.productRep = productRep;
     }
 
-    public List<Product> add(Product product){
+    public List<Product> getAllProducts() {
+        return productRep.findAll();
+    }
+
+    public void addProduct(Product product){
         productRep.save(product);
-        return  productRep.findAll();
+    }
+
+    public boolean editProduct(long id, Product updatedProduct) {
+        Optional<Product> existingProduct = productRep.findById(id);
+
+        if(existingProduct.isPresent()){
+            Product oldProduct = existingProduct.get();
+
+            oldProduct.setProductName(updatedProduct.getProductName());
+            oldProduct.setProductPrice(updatedProduct.getProductPrice());
+
+            productRep.save(oldProduct);
+            return true;
+        }
+        return false;
+    }
+
+    public void deleteProduct(long id) {
+        productRep.deleteById(id);
     }
 }
